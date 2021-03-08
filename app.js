bc5a8133874eabfafe1e4c86eebdb1a0a60c946b
sabynn <71257462+sabynn@users.noreply.github.com>
2021-03-08 09:46:52 +0700

    add rotate arrow

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 function openAnswer(n){
     var ans = document.getElementById("answer" + n);
+    var arrow = document.getElementById("arrow" + n);
     if (ans.style.display === "block") {
         ans.style.display = "none";
+        arrow.style.transform = "rotate(0)";
     } else {
         ans.style.display = "block";
+        arrow.style.transform = "rotate(180deg)";
         for (i=1; i<6;i++){
             if (i==n){
                 continue;
             }
             var j = document.getElementById("answer" + i);
+            var l = document.getElementById("arrow" + i);
             j.style.display = "none";
+            l.style.transform = "rotate(0)";
         }
     }
 }
